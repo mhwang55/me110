ba106d279555a2d4c51cc477bdf7bbe7197777f8
Max <mwang@rexchange.com>
2019-05-21 00:29:22 -0700

    added function

--- src/hexapod/src/tripod_gait.cpp
+++ src/hexapod/src/tripod_gait.cpp
@@ -26,6 +26,15 @@ sensor_msgs::JointState feedback;       // The actuator feedback struccture
 sensor_msgs::JointState initial_pos;
 double d = 5;
 
+float position (float initialPos, float desiredPos, int i)
+{
+  return initialPos + 
+         (desiredPos - initialPos) * 
+         (10*((i/10)*d*(i/10)*d*(i/10)*d) - 
+         15*((i/10)*d)*((i/10)*d*(i/10)*d*(i/10)*d) + 
+         6*((i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d));
+}
+
 
 /*
 **   Feedback Subscriber Callback
@@ -68,12 +77,22 @@ int main(int argc, char **argv)
   ros::ServiceClient add_group_client = n.serviceClient<AddGroupFromNamesSrv>("/hebiros/add_group_from_names");
   AddGroupFromNamesSrv add_group_srv;
   add_group_srv.request.group_name = group_name;
-  add_group_srv.request.names = {"hip1", "femur1", "tibia1", "hip2", "femur2", "tibia2",
-                                 "hip3", "femur3", "tibia3", "hip4", "femur4", "tibia4",
-                                 "hip5", "femur5", "tibia5", "hip6", "femur6", "tibia6"};
-  add_group_srv.request.families = {"leg1", "leg1", "leg1", "leg2", "leg2", "leg2", 
-                                    "leg3", "leg3", "leg3", "leg4", "leg4", "leg4",
-                                    "leg5", "leg5", "leg5", "leg6", "leg6", "leg6"};
+  add_group_srv.request.names = {"hip1", "femur1", "tibia1",
+                                 "hip2", "femur2", "tibia2",
+                                 "hip3", "femur3", "tibia3",
+                                 "hip4", "femur4", "tibia4",
+                                 "hip5", "femur5", "tibia5",
+                                 "hip6", "femur6", "tibia6"
+                                };
+
+  add_group_srv.request.families = {"leg1", "leg1", "leg1",
+                                    "leg2", "leg2", "leg2",
+                                    "leg3", "leg3", "leg3",
+                                    "leg4", "leg4", "leg4",
+                                    "leg5", "leg5", "leg5",
+                                    "leg6", "leg6", "leg6"
+                                   };
+
   // Repeatedly call the service until it succeeds.
   while(!add_group_client.call(add_group_srv)) ;
 
@@ -100,7 +119,7 @@ int main(int argc, char **argv)
   initial_pos.effort.reserve(leg_components);
 
   // Create publisher to send done commands. 
-  ros::Publisher done_publisher = n.advertise<
+  //ros::Publisher done_publisher = n.advertise<sensor_msgs::Boolean>("")
 
   // Create a publisher to send commands to the actuator group.
   ros::Publisher command_publisher = n.advertise<sensor_msgs::JointState>("/hebiros/"+group_name+"/command/joint_state", 100);
@@ -131,8 +150,8 @@ int main(int argc, char **argv)
   // Wait until we have some feedback from the actuator.
   ROS_INFO("Waiting for initial feedback");
   while (!feedbackvalid)  {
-      ros::spinOnce();
-      loop_rate.sleep();
+    ros::spinOnce();
+    loop_rate.sleep();
   }
 
   // Prep the servo loop.
@@ -140,75 +159,116 @@ int main(int argc, char **argv)
   double  speed = 1.0;          // Speed to reach goal.
   double  cmdpos = feedback.position[0];
   double  cmdvel = 0.0;
+  double  rad1, rad2, effort1, effort2, effortTibiaLeft, effortTibiaRight;
+  rad1 = M_PI/2;
+  rad2 = -M_PI/2;
+  ///*
+  rad1 = 0.7;
+  rad2 = -0.7;
+  //*/
 
-  while(ros::ok())  { 
+  effort1 = -1;
+  effort2 = 1;
+  ///*
+  effortTibiaLeft = 0;
+  effortTibiaRight = -0;
+  //*/
 
+  bool prep = false;
+
+  while(ros::ok())
+  {
     initial_pos = feedback;
 
-    for (int i=0; i<10; i++) {
-      command_msg.position[0] = 0;
-      command_msg.position[1] = initial_pos.position[1] + 
-                                (M_PI/2 - initial_pos.position[1]) * 
-                                (10*((i/10)*d*(i/10)*d*(i/10)*d) - 
-                                15*((i/10)*d)*((i/10)*d*(i/10)*d*(i/10)*d) + 
-                                6*((i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d));
-      command_msg.position[2] = 0;
-      command_msg.effort[0] = 0;
-      command_msg.effort[1] = 8;
-      command_msg.effort[2] = -5;
-      command_msg.position[3] = 0;
-      command_msg.position[4] = initial_pos.position[4] + 
-                                (-M_PI/2 - initial_pos.position[4]) * 
-                                (10*((i/10)*d*(i/10)*d*(i/10)*d) - 
-                                15*((i/10)*d)*((i/10)*d*(i/10)*d*(i/10)*d) + 
-                                6*((i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d));
-      command_msg.position[5] = 0;
-      command_msg.effort[3] = 0;
-      command_msg.effort[4] = -8;
-      command_msg.effort[5] = 5;
-      command_msg.position[6] = 0;
-      command_msg.position[7] = initial_pos.position[7] + 
-                                (M_PI/2 - initial_pos.position[7]) * 
-                                (10*((i/10)*d*(i/10)*d*(i/10)*d) - 
-                                15*((i/10)*d)*((i/10)*d*(i/10)*d*(i/10)*d) + 
-                                6*((i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d));
-      command_msg.position[8] = 0;
-      command_msg.effort[6] = 0;
-      command_msg.effort[7] = 8;
-      command_msg.effort[8] = -5;
-      command_msg.position[9] = 0;
-      command_msg.position[10] = initial_pos.position[10] + 
-                                (-M_PI/2 - initial_pos.position[10]) * 
-                                (10*((i/10)*d*(i/10)*d*(i/10)*d) - 
-                                15*((i/10)*d)*((i/10)*d*(i/10)*d*(i/10)*d) + 
-                                6*((i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d));
-      command_msg.position[11] = 0;
-      command_msg.effort[9] = 0;
-      command_msg.effort[10] = -8;
-      command_msg.effort[11] = 5;
-      command_msg.position[12] = 0;
-      command_msg.position[13] = initial_pos.position[13] + 
-                                (M_PI/2 - initial_pos.position[13]) * 
-                                (10*((i/10)*d*(i/10)*d*(i/10)*d) - 
-                                15*((i/10)*d)*((i/10)*d*(i/10)*d*(i/10)*d) + 
-                                6*((i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d));
-      command_msg.position[14] = 0;
-      command_msg.effort[12] = 0;
-      command_msg.effort[13] = 8;
-      command_msg.effort[14] = -5;
-      command_msg.position[15] = 0;
-      command_msg.position[16] = initial_pos.position[16] + 
-                                (-M_PI/2 - initial_pos.position[16]) * 
-                                (10*((i/10)*d*(i/10)*d*(i/10)*d) - 
-                                15*((i/10)*d)*((i/10)*d*(i/10)*d*(i/10)*d) + 
-                                6*((i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d*(i/10)*d));
-      command_msg.position[17] = 0;
-      command_msg.effort[15] = 0;
-      command_msg.effort[16] = -8;
-      command_msg.effort[17] = 5;
-
-      command_publisher.publish(command_msg);
-
+    if (!prep)
+    {
+      for (int i=0; i<10; i++)
+      {
+        command_msg.position[0] = 0;
+        command_msg.position[1] = position(initial_pos.position[1], rad1, i);
+        command_msg.position[2] = 0;
+        command_msg.effort[0] = 0;
+        command_msg.effort[1] = effort1;
+        command_msg.effort[2] = effortTibiaRight;
+        command_msg.position[3] = 0;
+        command_msg.position[4] = position(initial_pos.position[4], rad2, i);
+        command_msg.position[5] = 0;
+        command_msg.effort[3] = 0;
+        command_msg.effort[4] = effort2;
+        command_msg.effort[5] = effortTibiaLeft;
+        command_msg.position[6] = 0;
+        command_msg.position[7] = position(initial_pos.position[7], rad1, i);
+        command_msg.position[8] = 0;
+        command_msg.effort[6] = 0;
+        command_msg.effort[7] = effort1;
+        command_msg.effort[8] = effortTibiaRight;
+        command_msg.position[9] = 0;
+        command_msg.position[10] = position(initial_pos.position[10], rad2, i);
+        command_msg.position[11] = 0;
+        command_msg.effort[9] = 0;
+        command_msg.effort[10] = effort2;
+        command_msg.effort[11] = effortTibiaLeft;
+        command_msg.position[12] = 0;
+        command_msg.position[13] = position(initial_pos.position[13], rad1, i);
+        command_msg.position[14] = 0;
+        command_msg.effort[12] = 0;
+        command_msg.effort[13] = effort1;
+        command_msg.effort[14] = effortTibiaRight;
+        command_msg.position[15] = 0;
+        command_msg.position[16] = position(initial_pos.position[16], rad2, i);
+        command_msg.position[17] = 0;
+        command_msg.effort[15] = 0;
+        command_msg.effort[16] = effort2;
+        command_msg.effort[17] = effortTibiaLeft;
+  
+        command_publisher.publish(command_msg);
+      }
+    }
+    else
+    {
+  rad1 = M_PI/2;
+  rad2 = -M_PI/2;
+      for (int i=0; i<10; i++)
+      {
+        command_msg.position[0] = 0;
+        command_msg.position[1] = position(initial_pos.position[1], rad1, i);
+        command_msg.position[2] = 0;
+        command_msg.effort[0] = 0;
+        command_msg.effort[1] = 8;
+        command_msg.effort[2] = -5;
+        command_msg.position[3] = 0;
+        command_msg.position[4] = position(initial_pos.position[4], rad2, i);
+        command_msg.position[5] = 0;
+        command_msg.effort[3] = 0;
+        command_msg.effort[4] = -8;
+        command_msg.effort[5] = 5;
+        command_msg.position[6] = 0;
+        command_msg.position[7] = position(initial_pos.position[7], rad1, i);
+        command_msg.position[8] = 0;
+        command_msg.effort[6] = 0;
+        command_msg.effort[7] = 8;
+        command_msg.effort[8] = -5;
+        command_msg.position[9] = 0;
+        command_msg.position[10] = position(initial_pos.position[10], rad2, i);
+        command_msg.position[11] = 0;
+        command_msg.effort[9] = 0;
+        command_msg.effort[10] = -8;
+        command_msg.effort[11] = 5;
+        command_msg.position[12] = 0;
+        command_msg.position[13] = position(initial_pos.position[13], rad1, i);
+        command_msg.position[14] = 0;
+        command_msg.effort[12] = 0;
+        command_msg.effort[13] = 8;
+        command_msg.effort[14] = -5;
+        command_msg.position[15] = 0;
+        command_msg.position[16] = position(initial_pos.position[16], rad2, i);
+        command_msg.position[17] = 0;
+        command_msg.effort[15] = 0;
+        command_msg.effort[16] = -8;
+        command_msg.effort[17] = 5;
+  
+        command_publisher.publish(command_msg);
+      }
     }
 
     ros::spinOnce();
